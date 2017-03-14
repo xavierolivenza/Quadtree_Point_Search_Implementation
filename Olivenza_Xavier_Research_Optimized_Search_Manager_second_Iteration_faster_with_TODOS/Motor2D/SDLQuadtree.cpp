9291e102a8c46aa2c62bf20f38e613cc03cbb98f
xavierolivenza <yotolivenza@gmail.com>
2017-03-14 18:11:04 +0100

    TODO update

--- Olivenza_Xavier_Research_Optimized_Search_Manager_second_Iteration_faster_with_TODOS/Motor2D/SDLQuadtree.cpp
+++ Olivenza_Xavier_Research_Optimized_Search_Manager_second_Iteration_faster_with_TODOS/Motor2D/SDLQuadtree.cpp
@@ -6,11 +6,11 @@ std::vector<SDL_Rect> QuadtreeAABBs;
 // Insert new point in the quadtree
 bool AABB::Insert(iPoint* newpoint)
 {
-	// TODO 1: If new point is not in the quadtree AABB, return
+	// TODO 1: The new point is inside the quadtree AABB?
 
-	// TODO 2: If in this node there is space for the point, pushback it (remember Max_Elements_in_Same_Node)
+	// TODO 2: Add the point to the node if there is space for it (remember Max_Elements_in_Same_Node)
 
-	// TODO 5: Otherwise, subdivide and add the point to one of the new nodes
+	// TODO 5: If there is not space, add the point to a subdivision
 
 	return false;
 }
@@ -22,21 +22,21 @@ void AABB::subdivide()
 
 	// TODO 4: Now we have all the positions and size of each number we 
 	// can create each child with its AABB
-	// ! Don't forget to set child root!
+	// Don't forget to set child root
 }
 
 int AABB::CollectCandidates(std::vector< iPoint* >& nodes, const SDL_Rect& r)
 {
 	uint ret = 0;
 
-	// TODO 6: If range is not in the quadtree, return
+	// TODO 6: Check if range is in the quadtree AABB
 
-	// TODO 7: See if the points of this node are in range and pushback them to the vector
+	// TODO 7: See if the points of this node are in range, get them
 	//(remember Max_Elements_in_Same_Node and ret to count how many points we check)
 
-	// TODO 8: If there is no children, end
+	// TODO 8: If the node don't hace children, we can end
 
-	// TODO 9: Otherwise, add the points from the children (Recursive)
+	// TODO 9: If the noide has children, get them points
 
 	return ret;
 }
